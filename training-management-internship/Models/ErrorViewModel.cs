02caeffe248681188d22652e9bb1fb5cd0b96431
IzumiDKC <huudien111@gmail.com>
2025-05-14 22:49:10 +0700

    Initial commit

---
+++ training-management-internship/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace training_management_internship.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
