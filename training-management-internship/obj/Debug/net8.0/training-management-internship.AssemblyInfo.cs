dddf5f31d51171f59779045d5b504c9efb0dd143
IzumiDKC <huudien111@gmail.com>
2025-06-18 22:15:06 +0700

    Find user by email (login) -> check result when logging in console Reset local user (condition that Account is not bound to ChiTietLop), handle admin account: Reconfigure DbInitializer.cs File to receive Admin Account when running Remove User controller to merge into Admin Controller (use [Authorize])

--- training-management-internship/obj/Debug/net8.0/training-management-internship.AssemblyInfo.cs
+++ training-management-internship/obj/Debug/net8.0/training-management-internship.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("training-management-internship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dde7a0a8b93ef829e45650008c5c6ebf6eb9963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb04e7729c3b9d817073ec2478a4e68cb2c436fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("training-management-internship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("training-management-internship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
